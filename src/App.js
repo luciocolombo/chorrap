9d2e3c3bb790c00e7393aa7ebe4e4c7835229ac4
lucio <colombolucio@hotmail.com>
2021-03-15 18:45:15 -0300

    de

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react'
 import './App.css';
-
+import { Button } from 'react-bootstrap';
 import Map from './Map'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 /* import { Tabs, Tab, Sonet } from 'react-bootstrap';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom"; */
@@ -15,7 +15,7 @@ return(
 
   <div className="App">
     <h1 className="hh1">Juntas contra el acoso callejero</h1>
-    <button onClick={()=>{toggleDraggableVisibility(!draggableVisibility)}}>Toggle draggable marker visibility</button>
+    <Button className="btn btn-info" onClick={()=>{toggleDraggableVisibility(!draggableVisibility)}}>Toggle draggable marker visibility (and show all crimes)</Button>
     <Map showDraggable={draggableVisibility}/>
   </div>
 )
--- src/Map.js
+++ src/Map.js
@@ -2,41 +2,48 @@ import React, {useState} from 'react'
 import { MapContainer, TileLayer } from 'react-leaflet'
 /* import CustomMarker from './CustomMarker' */
 import DraggableMarker from './DraggableMarker'
+import { Button } from 'react-bootstrap';
 /* import axios from "axios"; */
+
+
+
 function Map({showDraggable}) {
     const [draggablePosition, setDraggablePosition]=useState("")
+
     function DraggableMarkerPosition(position){
         setDraggablePosition(position)
         console.log("posicion actual draggableposition", draggablePosition)
     }
+
     function sendDraggableToDb(){
         console.log("Send draggable to db")
        /*  const crimePosition=draggablePosition */
 /*         axios.post("URL",crimePosition) */
     }
    const position = [-32.959676, -60.661406]
+
+   
     return (
         <div>
-            {showDraggable&&<button onClick={sendDraggableToDb}>Save marker position</button>}
-            
-        <MapContainer className="mapcontainer" center={position} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-         {/*    <CustomMarker position={position} text="Custom crime info"/> */}
-         
-        {showDraggable?
-          (<div className="btns">
-              <DraggableMarker savePosition={DraggableMarkerPosition}/>
-              
-            </div>
-             ):
-          (console.log("draggable visibility off"))
-         }
-
-        
-        </MapContainer>
+            {showDraggable?
+            <Button className="btn btn-info" onClick={sendDraggableToDb}>Save marker position</Button>:
+            <Button className="btn btn-info" disabled>Save marker position</Button>}
+                
+            <MapContainer className="mapcontainer" center={position} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {/*    <CustomMarker position={position} text="Custom crime info"/> */}
+                    
+                    {showDraggable?
+                    (<div className="btns">
+                        <DraggableMarker savePosition={DraggableMarkerPosition}/>
+                    </div>
+                        ):
+                    (console.log("draggable visibility off"))
+                }
+            </MapContainer>
         </div>
     )
 }
