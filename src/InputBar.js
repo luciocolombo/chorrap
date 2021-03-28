a88181b9b71a5d7e1c7f0a323886ab3b6f1eee5c
luciocolombo <colombolucio@hotmail.com>
2021-03-28 07:48:57 -0300

    fisrt info

--- src/InputBar.js
+++ src/InputBar.js
@@ -1,42 +1,75 @@
 import React, {useState} from 'react'
-import {Form, Button} from 'react-bootstrap'
+import {Form, Button, Modal} from 'react-bootstrap'
 import Map from './Map'
 
 function InputBar() {
+
+    const [position, setPosition]=useState("");
+    const [email, changeEmail] = useState("");
+    const [raza, changeRaza]=useState("");
+    const [blackColor, toggleBlackColor]= useState(false);
+    const [whiteColor, toggleWhiteColor]= useState(false);
+    const [brownColor, toggleBrownColor]= useState(false);
+    const [blondeColor, toggleBlondeColor]= useState(false);
+    const [redColor, toggleRedColor]= useState(false);
+    
+    
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    
+    function sendToDb(){
+        var dog={position, email,raza,blackColor,whiteColor,brownColor,blackColor,redColor}
+    /*   axios.post("URL",dogInfo) */
+      console.log(dog)
+      handleClose()
+  }
    /*  const [draggableVisibility, toggleDraggableVisibility]=useState(false) */
     return (
         <div>
             <h1 className="hh1">Encontraste un perro perdido en Rosario?</h1>
             <h2>Completa los datos para comunicarlo a la comunidad. No olvides marcar la posición en el mapa</h2>
-            
+            {/*PORQUE NO SE IMPRIME ESTO? <p>{email, raza, blackColor, "asodkasod"}</p> */}
             <Form>
-                <Form.Group controlId="formBasicEmail">
+                <Form.Group controlId="formBasicEmailRaza">
                     <Form.Label>Email de contacto</Form.Label>
-                    <Form.Control type="email" placeholder="Ingrese email" />
+                    <Form.Control type="email" placeholder="Ingrese email" value={email} onChange={(e)=>changeEmail(e.target.value)} />
                     <Form.Label>Conoces la raza del perro?</Form.Label>
-                    <Form.Control type="String" placeholder="Introduzca raza si la conoce" />
+                    <Form.Control type="String" placeholder="Introduzca raza si la conoce" value={raza} onChange={(e)=>changeRaza(e.target.value)}/>
                 </Form.Group>
 
                 <Form.Group controlId="formBasicCheckbox">
                     <h2>Seleccione hasta 3 colores de perro</h2>
-                    <Form.Check type="checkbox" label="Blanco" />
-                    <Form.Check type="checkbox" label="Negro" />
-                    <Form.Check type="checkbox" label="Marron" />
-                    <Form.Check type="checkbox" label="Rubio" />
-                    <Form.Check type="checkbox" label="Colorado" />
+                    <Form.Check type="checkbox" label="Negro" value={blackColor} onChange={(e)=>toggleBlackColor(e.target.checked)}/> 
+                    <Form.Check type="checkbox" label="Blanco" value={whiteColor} onChange={(e)=>toggleWhiteColor(e.target.checked)}/>
+                    <Form.Check type="checkbox" label="Marron" value={brownColor} onChange={(e)=>toggleBrownColor(e.target.checked)}/>
+                    <Form.Check type="checkbox" label="Rubio" value={blondeColor} onChange={(e)=>toggleBlondeColor(e.target.checked)}/>
+                    <Form.Check type="checkbox" label="Colorado" value={redColor} onChange={(e)=>toggleRedColor(e.target.checked)}/>
                 </Form.Group>
 
                 <Form.Group>
                     <h2>Subir imagen del perro</h2>
                     <Form.File id="exampleFormControlFile1" label="Example file input" />
                 </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit to DB
-                    
-                </Button>
+                <Button onClick={handleShow}> Send to DB
+                </Button>                
+                <Modal show={show} onHide={handleClose}>
+                            <Modal.Header closeButton>
+                                <Modal.Title>Sending dog...</Modal.Title>
+                            </Modal.Header>
+
+                            <Modal.Body>
+                                <p>Send dog to DB?</p>
+                            </Modal.Body>
+
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={handleClose}>Close</Button>
+                                <Button variant="primary" type="submit" onClick={sendToDb}>Save to DB</Button>
+                            </Modal.Footer>
+                </Modal>
                {/*  <Button className="btn btn-info ml-3 mb-1" onClick={()=>{toggleDraggableVisibility(!draggableVisibility)}}>Toggle draggable marker visibility (and show all lost dogs)</Button> */}
             </Form>
-            <Map /* showDraggable={draggableVisibility} *//>
+            <Map setPosition={setPosition}/* showDraggable={draggableVisibility} *//>
            
         </div>
         
--- src/Map.js
+++ src/Map.js
@@ -7,29 +7,30 @@ import axios from "axios"
 
 
 
-function Map({showDraggable}) {
+function Map({setPosition}/* {showDraggable} */) {
     const [draggablePosition, setDraggablePosition]=useState("")
 
     function DraggableMarkerPosition(position){
         setDraggablePosition(position)
-        console.log("posicion actual draggableposition", draggablePosition)
+      /*   console.log("posicion actual draggableposition", draggablePosition) */
+        setPosition(draggablePosition)
     }
 
-    function sendDraggableToDb(){
+    /* function sendDraggableToDb(){
         console.log("Send draggable to db")
-       /*  const crimePosition=draggablePosition
+         const crimePosition=draggablePosition
         axios.post("http://localhost:4000/postcrime",crimePosition)
          .then(console.log("ENVIADO ")
-        ) */
-    }
+        ) 
+    } */
    const position = [-32.959676, -60.661406]
 
    
     return (
         <div>
-            {showDraggable?
+           {/*  {showDraggable?
             <Button className="btn btn-info ml-3" onClick={sendDraggableToDb}>Enviar perro perdido a DB</Button>:
-            <Button className="btn btn-info ml-3" disabled>Enviar perro perdido a DB</Button>}
+            <Button className="btn btn-info ml-3" disabled>Enviar perro perdido a DB</Button>} */}
                 
             <MapContainer className="mapcontainer" center={position} zoom={13} scrollWheelZoom={false}>
                 <TileLayer
@@ -38,13 +39,13 @@ function Map({showDraggable}) {
                     />
                     {/*    <CustomMarker position={position} text="Custom crime info"/> */}
                     
-                    {showDraggable?
-                    (<div className="btns">
+                    {/* {showDraggable?
+                    (<div className="btns"> */}
                         <DraggableMarker savePosition={DraggableMarkerPosition}/>
-                    </div>
+               {/*      </div>
                         ):
                     (console.log("draggable visibility off"))
-                }
+                } */}
             </MapContainer>
         </div>
     )
