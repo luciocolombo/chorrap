2b297ea6559125cec88f80d849e3fd1ee2f66d5b
luciocolombo <colombolucio@hotmail.com>
2021-03-31 09:02:21 -0300

    d

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Button } from 'react-bootstrap';
 import {BrowserRouter as Router, Route, Redirect} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import InputBar from "./InputBar"
+import MapAllDogs from "./MapAllDogs"
 /* import { Tabs, Tab, Sonet } from 'react-bootstrap';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom"; */
@@ -15,10 +16,13 @@ function App() {
 return(
 
     <Router>
-      <Route path="/" /* exact */>
+      <Route path="/"  exact >
         <div className="App">
           <InputBar/>
         </div>
+      </Route>
+      <Route path="/all" exact>
+        <MapAllDogs />
       </Route>
     </Router>
 
