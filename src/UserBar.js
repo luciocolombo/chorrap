b7c374357dacd7633ae60dcadec6f5921c240be3
luciocolombo <colombolucio@hotmail.com>
2021-05-26 15:20:24 -0300

    more

--- src/UserBar.js
+++ src/UserBar.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Button} from 'react-bootstrap'
+import { useHistory } from "react-router-dom"
 
 function UserBar() {
+    let history = useHistory();
+    function onClick(){
+        localStorage.clear();
+        document.cookie = `user=${localStorage.getItem("user")}; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`
+        history.push('/login')
+    }
     return (
         <div>
+            <Button onClick={onClick}>Log off</Button>
             <p className="text-right">Logged as {localStorage.getItem("user")?localStorage.getItem("user"):"n/a"}</p>
         </div>
     )
