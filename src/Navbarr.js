8e10f7049e04035241704b51e09e58616f862777
luciocolombo <colombolucio@hotmail.com>
2021-06-08 14:35:02 -0300

    latest

--- src/Navbarr.js
+++ src/Navbarr.js
@@ -3,16 +3,10 @@ import React from 'react';
 function Navbarr() {
   return (
     <div className="d-flex my-4 navbarra">
-      <a
-        href="https://mascotasperdidasapi.herokuapp.com/dogs"
-        className="d-block btn btn-primary mr-3 navbarra"
-      >
+      <a href="/login" className="d-block btn btn-primary mr-3 navbarra">
         <i className="fas fa-dog"></i> Reporta mascotas
       </a>
-      <a
-        href="https://mascotasperdidasapi.herokuapp.com/dogs"
-        className="d-block btn  btn-primary navbarra"
-      >
+      <a href="/login" className="d-block btn  btn-primary navbarra">
         <i className="fas fa-paw"></i> Encontá tu mascota
       </a>
     </div>
